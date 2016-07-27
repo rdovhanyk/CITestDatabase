a8b8d4e7fdf4aa0745aeebd98a73358dbb99eb99
Roman Dovhanyk <roman.dohanyk@gmail.com>
2016-07-27 14:11:20 +0300

    Test change 2

--- CITestDatabase/stage/Stored Procedures/CleanStage.sql
+++ CITestDatabase/stage/Stored Procedures/CleanStage.sql
@@ -6,12 +6,9 @@ BEGIN
 	TRUNCATE TABLE [stage].[FBAds];
 	TRUNCATE TABLE [stage].[FBObjectStorySpec];
 	TRUNCATE TABLE [stage].[FBInsights];
-
 	TRUNCATE TABLE [stage].[CakeConversions];
-
 	TRUNCATE TABLE [stage].[SHLCampaignSummary];
 	TRUNCATE TABLE [stage].[SHLConversions];
-
 	TRUNCATE TABLE [stage].[A4DCampaignSummary];
 	TRUNCATE TABLE [stage].[A4DConversions];
 	
