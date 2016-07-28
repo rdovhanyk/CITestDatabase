04d8418b9aaa8efd56b4ede15841327c53427cb3
Roman Dovhanyk <roman.dohanyk@gmail.com>
2016-07-28 12:00:51 +0300

    Test change

--- CITestDatabase/stage/Stored Procedures/Load_CakeCampaignInsightsUTC.sql
+++ CITestDatabase/stage/Stored Procedures/Load_CakeCampaignInsightsUTC.sql
@@ -3,7 +3,7 @@
 AS
 BEGIN
 	SET DATEFIRST 7
-	/* Test comment 5 */
+	/* Test comment 6 */
 	DECLARE @USDLS smallint, @cake_timezone_offset_hours_utc int
 	
 	SELECT @cake_timezone_offset_hours_utc = SettingValue FROM [dbo].[Settings] WHERE SettingName = 'CAKETimeZoneOffsetHoursUTC'
